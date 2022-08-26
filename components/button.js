0525ac4bb1b99831dac91be1d75763542bc4e111
James Lovallo <lovallo.james@gmail.com>
2022-08-26 16:48:19 -0400

    button  updates

--- components/button.js
+++ components/button.js
@@ -1,4 +1,3 @@
-import css from './utils/css-prop'
 import link from './utils/link'
 
 export default {
@@ -10,64 +9,14 @@ export default {
 			icon: JSON.parse,
 			label: String,
 			link: link,
-			rounded: JSON.parse,
-			stretch: JSON.parse,
 			trailing_icon: JSON.parse,
-			variant: this.set_variant,
-		}
-	},
-	set_variant(v) {
-		const invert = '--background: transparent; --color: var(--button);'
-		switch (v) {
-			case 'icon':
-				return /* css */ `
-					:host {
-						background: var(--app-bar);
-						border-radius: 24px;
-						color: var(--on-app-bar);
-						height: 48px;
-						width: 48px;
-					}
-					button {
-						padding: 0;
-					}
-					svg { height: 24px; width: 24px; }
-				`
-			case 'outlined':
-				return /* css */ `
-						:host { 
-							${invert}
-							box-shadow: 0 0 0 1px var(--button);
-							margin: 1px; 
-						}
-					`
-			case 'raised':
-				return /* css */ `
-						:host { 
-							box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
-								0px 2px 2px 0px rgb(0 0 0 / 14%),
-								0px 1px 5px 0px rgb(0 0 0 / 12%);
-						}
-					`
-			case 'subtle':
-				return /* css */ `
-						:host { color: var(--button); background: transparent; }
-						[part=background] {
-							opacity: 0.1;
-							--hover-opacity: 0.2;
-							--active-opacity: 0.3;
-						}
-					`
-			case 'text':
-				return `:host { ${invert} --padding: 0 8px; }`
 		}
 	},
 	template() {
 		const tag = this.link ? 'a' : 'button'
 		const icon_only = this.getAttribute('variant') === 'icon'
 		return /* html */ `
-			<span part="background"></span>
-			<${tag}
+			<${tag} part="button"
 				${this.link ? ` href="${this.link}"` : ''}
 				${icon_only ? ` aria-label="${this.label}"` : ''}
 			>
@@ -79,58 +28,124 @@ export default {
 	styles() {
 		return /* css */ `
 			:host {
-				${css('border-radius', this.rounded ? '18px' : '4px')}
-				${css('flex-grow', this.stretch ? 1 : 0)}
 				--button: var(--${this.color});
 				--on-button: var(--on-${this.color});
-				--background: var(--button);
-				--color: var(--on-button);
-				background: var(--background);
-				color: var(--color);
-				display: inline-grid;
-				min-height: 36px;
+				border-radius: 4px;
+				display: grid;
+				height: 36px;
 				overflow: hidden;
+				width: max-content;
 			}
-			[part=background] {
-				background: currentcolor;
-				opacity: 0;
-			}
-			:host(:hover) [part=background] {
-				opacity: var(--hover-opacity);
-			}
-			:host(:active) [part=background] {
-				opacity: var(--active-opacity);
-			}
-			[part=background], a, button {
-				grid-area: 1/-1;
-			}
-			a, button {
-				${this.trailing_icon ? 'flex-direction: row-reverse;' : ''}
-				${css('justify-content', this.stretch ? 'space-between' : 'center')}
+
+			/* the link itself */
+
+			[part=button] {
 				align-items: center;
-				background: none;
+				background: transparent;
 				border: none;
 				color: inherit;
 				cursor: pointer;
 				display: flex;
-				font-family: var(--ui-family);
+				font-family: system-ui;
 				font-size: 14px;
 				font-weight: 500;
-				gap: 8px;
+				grid-area: 1/-1;
 				justify-content: center;
 				letter-spacing: 1.25px;
-				padding: var(--padding, 0 16px);
+				padding: 0 8px;
 				text-decoration: none;
 				text-transform: uppercase;
 				user-select: none;
-				z-index: 1;
 			}
+
+			/* background pseudo element */
+
+			:host:before {
+				background: var(--button-bg, currentcolor);
+				content: '';
+				display: flex;
+				grid-area: 1/-1;
+				opacity: 0;
+				pointer-events: none;
+			}
+
+			:host(:hover):before {
+				opacity: var(--hover-opacity);
+			}
+
+			:host(:active):before {
+				opacity: var(--active-opacity);
+			}
+
+			/* unelevated and raised */
+
+			:host([variant=unelevated]),
+			:host([variant=raised]) {
+				--button-bg: var(--on-button);
+				background: var(--button);
+				color: var(--on-button);
+			}
+
+			/* raised */
+
+			:host([variant=raised]) {
+				box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+					0px 2px 2px 0px rgb(0 0 0 / 14%),
+					0px 1px 5px 0px rgb(0 0 0 / 12%)
+			}
+
+			/* outlined */
+
+			:host([variant=outlined]) {
+				--button-bg: var(--button);
+				box-shadow: inset 0 0 0 2px var(--button);
+				color: var(--button);
+			}
+
+			/* subtle */
+
+			:host([variant=subtle]) {
+				--button-bg: var(--button);
+				color: var(--button);
+			}
+			:host([variant=subtle]):before {
+				opacity: var(--hover-opacity);
+			}
+			:host([variant=subtle]:hover):before {
+				opacity: calc(2 * var(--hover-opacity));
+			}
+			:host([variant=subtle]:active):before {
+				opacity: calc(2 * var(--active-opacity));
+			}
+
+			/* text */
+
+			:host([variant=text]) {
+				--button-bg: var(--button);
+				color: var(--button);
+			}
+
+			/* rounded */
+
+			:host([rounded=true]) {
+				border-radius: 18px;
+			}
+
+			:host([rounded=true]) [part=button] {
+				padding: 0 16px;
+			}
+
+			/* stretch */
+
+			:host([stretch=true]) {
+				flex-grow: 1;
+			}
+
 			svg {
 				fill: currentcolor;
 				height: 18px;
 				width: 18px;
 			}
-			${this.variant}
 		`
 	},
 }
